c2218b69b1fa368236a7e744daa7564b20df26bf
Meka <nmeka@deloitte.com>
2020-01-23 07:56:31 +0530

    second method banner export

--- src/components/Header/Baneer.js
+++ src/components/Header/Baneer.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { name } from "../../data";
 //rfc
-export default function Baneer() {
+// export default function Baneer() {
+//   return (
+//     <div>
+//       <h1>hello banner component{name}</h1>
+//     </div>
+//   );
+// }
+
+function Banner() {
   return (
     <div>
-      <h1>hello banner component{name}</h1>
+      <h1>Hello From Banner {name} </h1>
     </div>
   );
 }
+
+export default Banner;
