754f8c8ab641827468a88d4b05e74eb9d26bb0f2
Meka <nmeka@deloitte.com>
2020-01-23 07:35:06 +0530

    export/import

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// named and default import/exports
+//only one default export module
+//think of module as file
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import { name, age, person } from "./data";
+//import Banner from "./components/Header/Banner";
+
+const App = () => (
+  <section>
+    {/* //<Banner /> */}
+    <p>this is my content</p>
+    <p>{name}</p>
+    <p>{age}</p>
+    <p>{person.name}</p>
+  </section>
+);
+
+// import * as data from "./data";
+// const App = () => (
+//   <section>
+//     <p>this is my content</p>
+//     <p>{data.name}</p>
+//     <p>{data.age}</p>
+//     <p>{data.person.name}</p>
+//   </section>
+// );
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const name = "john";
+export const age = 27;
+
+export const person = {
+  name: "peter",
+  age: 25
+};
+//const secretValue = "secret";
+
+// const name = "john";
+// const age = 27;
+
+// const person = {
+//  name: "peter",
+//  age: 25
+// };
+const secretValue = "secret";
