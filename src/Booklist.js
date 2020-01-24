ed356476535b646b96530f91f2856f7ab78d2772
Meka <nmeka@deloitte.com>
2020-01-24 07:44:06 +0530

    statedisplayimg in log

--- src/Booklist.js
+++ src/Booklist.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
 import Book from "./Book";
 export default class Booklist extends Component {
-  books = [
-    {
-      book: "book number one",
-      author: "john doe"
-    },
-    {
-      book: "book number two",
-      author: "bobby doe"
-    }
-  ];
+  state = {
+    books: [
+      {
+        book: "book number one",
+        author: "john doe"
+      },
+      {
+        book: "book number two",
+        author: "bobby doe"
+      }
+    ]
+  };
   render() {
+    const books = this.state.books;
+    console.log(books);
     return (
       <section>
         <h3>This is our book list</h3>
-        <Book book={this.books[0]} />
-        <Book book={this.books[1]}></Book>
       </section>
     );
   }
