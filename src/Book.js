dd60979553be5c540a61900719ee2c3cdac9bf30
Meka <nmeka@deloitte.com>
2020-01-24 16:02:14 +0530

    button delete

--- src/Book.js
+++ src/Book.js
@@ -1,33 +1,27 @@
 import React, { Component } from "react";
-
+import Button from "./Button";
+// PROP DRILLING PASSING METHODS TO CHILDREN
 export default class Book extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 1
+      count: 1,
+      name: "john"
     };
-    //this.handleClick = this.handleClick.bind(this);
   }
-  // handleClick() {
-  //   console.log("clicked me");
-  //   console.log(this.state.count);
-  // }
-  handleClick = () => {
-    console.log("yiu clicked me");
-    console.log(this.state.count);
-  };
 
   render() {
     const { img, title, author } = this.props.info;
+    const { handleDelete } = this.props;
+
     return (
       <article className="book">
         <img src={img} width="150" alt="book" />
         <div>
-          <h3>Title:{title}</h3>
-          <h5>Author:{author}</h5>
-          <button type="button" onClick={this.handleClick}>
-            add to cart
-          </button>
+          <h4>Title : {title}</h4>
+          <h6>Author : {author}</h6>
+
+          <Button handleDelete={handleDelete} />
         </div>
       </article>
     );
--- src/Booklist.js
+++ src/Booklist.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
 import Book from "./Book";
-import booksdata from "./Bookdata";
+import booksData from "./Bookdata";
 export default class Booklist extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: booksdata
+      books: booksData
     };
   }
 
+  handleDelete = () => {
+    console.log(`i'm from parent component`);
+  };
+
   render() {
-    // const books = this.state.books.map(item => item.book);
-    //console.log(books);
     return (
       <section>
-        <h3>This is our book list</h3>
-        {this.state.books.map((item, index) => (
-          <Book key={item.id} info={item} />
+        <h3>This is our Booklist</h3>
+        {this.state.books.map(item => (
+          <Book key={item.id} info={item} handleDelete={this.handleDelete} />
         ))}
       </section>
     );
