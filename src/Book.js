cae9b37ab8e073e8b6eccc6949aed83c59509feb
Meka <nmeka@deloitte.com>
2020-01-26 19:56:28 +0530

    toggle with ternery operator

--- src/Book.js
+++ src/Book.js
@@ -29,13 +29,20 @@ export default class Book extends Component {
           <button type="button" onClick={this.handleInfo}>
             Toggle info
           </button>
-          {this.state.showInfo && (
+          {this.state.showInfo ? (
             <p>
               In publishing and graphic design, Lorem ipsum is a placeholder
               text commonly used to demonstrate the visual form of a document or
               a typeface without relying on meaningful conten
             </p>
-          )}
+          ) : null}
+          {/* {this.state.showInfo && (
+            <p>
+              In publishing and graphic design, Lorem ipsum is a placeholder
+              text commonly used to demonstrate the visual form of a document or
+              a typeface without relying on meaningful conten
+            </p>
+          )} */}
         </div>
       </article>
     );
